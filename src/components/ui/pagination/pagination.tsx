89cdfa5a984f67d2f80a55a6cae3661848932e5d
melion11 <gphooenix@gmail.com>
2023-12-27 13:24:26 +0300

    add: completed pagination

--- src/components/ui/pagination/pagination.tsx
+++ src/components/ui/pagination/pagination.tsx
@@ -3,24 +3,36 @@ import { ComponentPropsWithoutRef, ElementRef, forwardRef } from 'react'
 import { clsx } from 'clsx'
 
 import { ArrowLeft, ArrowRight } from '../../../assets'
+import { CustomSelect, SelectOption } from '../select'
+import { Typography } from '../typography'
 
 import s from './pagination.module.scss'
-import { DOTS, rangeNumbers } from './rangeNumbers.ts'
+import { usePagination } from './usePagination.ts'
 
 export type PaginationProps = {
   totalCount: number
   itemsPerPage: number
   currentPage: number
   siblingCount?: number
+  options?: SelectOption[]
+  onChangeSize?: (pageSize: number) => void
   onChangePage?: (currentPage: number | string) => void
 } & ComponentPropsWithoutRef<'div'>
 
 export const Pagination = forwardRef<ElementRef<'div'>, PaginationProps>(
   (props: PaginationProps, ref, ...restProps) => {
-    const { totalCount, siblingCount = 1, itemsPerPage, currentPage, onChangePage } = props
+    const {
+      totalCount,
+      options,
+      onChangeSize,
+      siblingCount = 1,
+      itemsPerPage,
+      currentPage,
+      onChangePage,
+    } = props
     const pagesCount = Math.ceil(totalCount / itemsPerPage)
 
-    const pageNumbers = rangeNumbers(siblingCount, currentPage, pagesCount)
+    const pageNumbers = usePagination({ totalPageCount: pagesCount, currentPage, siblingCount })
 
     const onChangePageHandler = (currentPage: number | string) => {
       onChangePage?.(currentPage)
@@ -34,8 +46,13 @@ export const Pagination = forwardRef<ElementRef<'div'>, PaginationProps>(
       if (currentPage < pagesCount) onChangePage?.(currentPage + 1)
     }
 
+    const onChangeSizeHandler = (pageSize: string) => {
+      onChangeSize?.(+pageSize)
+    }
+
     const classNames = {
       root: s.Root,
+      paginationWrapper: s.paginationWrapper,
       pagesWrapper: s.pagesWrapper,
       prevBtn: clsx(s.btn),
       nextBtn: clsx(s.btn),
@@ -43,36 +60,55 @@ export const Pagination = forwardRef<ElementRef<'div'>, PaginationProps>(
 
     return (
       <div className={classNames.root} {...restProps} ref={ref}>
-        <button
-          disabled={currentPage === 1}
-          onClick={onChangeLeftPageHandler}
-          className={classNames.prevBtn}
-        >
-          <ArrowLeft />
-        </button>
-        <div className={classNames.pagesWrapper}>
-          {pageNumbers.map(page => {
-            return (
-              <button
-                onClick={() => onChangePageHandler(page)}
-                key={page}
-                className={`${s.page} 
+        <div className={classNames.paginationWrapper}>
+          <button
+            disabled={currentPage === 1}
+            onClick={onChangeLeftPageHandler}
+            className={classNames.prevBtn}
+            tabIndex={0}
+          >
+            <ArrowLeft />
+          </button>
+          <div className={classNames.pagesWrapper}>
+            {pageNumbers.map((page, i) => {
+              return (
+                <button
+                  onClick={() => onChangePageHandler(page)}
+                  key={i}
+                  tabIndex={0}
+                  className={`${s.page} 
                 ${page === currentPage ? s.active : ''} 
-                ${page === DOTS ? s.dots : s.page}`}
-                disabled={page === DOTS}
-              >
-                {page}
-              </button>
-            )
-          })}
+                ${page === '...' ? s.dots : s.page}`}
+                  disabled={page === '...'}
+                >
+                  {page}
+                </button>
+              )
+            })}
+          </div>
+          <button
+            disabled={currentPage === pagesCount}
+            tabIndex={0}
+            onClick={onChangeRightPageHandler}
+            className={classNames.nextBtn}
+          >
+            <ArrowRight />
+          </button>
         </div>
-        <button
-          disabled={currentPage === pagesCount}
-          onClick={onChangeRightPageHandler}
-          className={classNames.nextBtn}
-        >
-          <ArrowRight />
-        </button>
+        <div className={s.selectWrapper}>
+          <Typography as={'span'} variant={'body2'} className={s.selectText}>
+            Show
+          </Typography>
+          <CustomSelect
+            className={s.select}
+            value={itemsPerPage.toString()}
+            options={options}
+            onValueChange={onChangeSizeHandler}
+          />
+          <Typography as={'span'} variant={'body2'} className={s.selectText}>
+            on the page
+          </Typography>
+        </div>
       </div>
     )
   }
